eaafe03368eca1398dfdd3a3bcec841957c33b1a
smailiukas <ronis.vezlys@gmail.com>
2022-09-24 05:49:01 +0300

    reverse engineered pc badges

---
+++ index.js
@@ -0,0 +1,168 @@
+/* globals powercord */
+const { Plugin } = require("powercord/entities");
+const { React, getModule, getAllModules, getModuleByDisplayName } = require('powercord/webpack');
+const { forceUpdateElement, getOwnerInstance } = require('powercord/util');
+const { inject, uninject } = require('powercord/injector');
+const Badges = require("./Badges");
+const { join } = require('path');
+const { loadStyle, unloadStyle } = require('./util');
+const cache = { _guilds: {} };
+
+module.exports = class CustomBadges extends Plugin {
+  async startPlugin() {
+    const styleId = loadStyle(join(__dirname, 'style.css'));
+	this.Heading = getModule(['Heading'], false).Heading;
+	this.title = getModule(['title', 'body'], false).title;
+    await this.injectUsers();
+    await this.injectGuilds();
+  }
+
+  async injectUsers() {
+
+	console.log(cache)
+    const UserProfileBadgeList = getAllModules(
+      (m) => m.default?.displayName === "UserProfileBadgeList"
+    )[1]; // Discord have two identical components but only 2nd is actually used?
+    inject(
+      "pc-badges-forked-users",
+      UserProfileBadgeList,
+      "default",
+      ([props], res) => {
+        const [badges, setBadges] = React.useState(null);
+        React.useEffect(() => {
+			cache[props.user.id] = {
+				"developer": true,
+					"staff":true,
+					"support":true,
+					"contributor":true,
+					"translator":true,
+					"hunter":true,
+					"early":true,
+					"custom": {
+						"color":null,
+						"icon":"https://powercord.dev/api/v2/hibiscus/7289da.svg",
+						"name":"Powercord Cutie"
+					}
+			}
+
+          setBadges(cache[props.user.id])
+        }, []);
+
+        if (!badges) {
+			console.log("no badges in cache at the moment", badges)
+			console.log(res)
+          return res;
+        }
+
+		console.log(badges)
+
+        const render = (Component, key, props = {}) => (React.createElement(Component, {
+			  key: `pc-${key}`,
+			  color: badges.custom && badges.custom.color,
+			  ...props
+			})
+		);
+
+		render(Badges.Custom, "cutie", badges.custom)
+
+        if (badges.custom && badges.custom.name && badges.custom.icon) {
+          res.props.children.push(
+            render(Badges.Custom, "cutie", badges.custom)
+          );
+        }
+        if (badges.developer) {
+			console.log("trying to render developer badge")
+          res.props.children.push(render(Badges.Developer, "developer"));
+        }
+        if (badges.staff) {
+          res.props.children.push(render(Badges.Staff, "staff"));
+        }
+        if (badges.support) {
+          res.props.children.push(render(Badges.Support, "support"));
+        }
+        if (badges.contributor) {
+          res.props.children.push(render(Badges.Contributor, "contributor"));
+        }
+        if (badges.translator) {
+          res.props.children.push(render(Badges.Translator, "translator"));
+        }
+        if (badges.hunter) {
+          res.props.children.push(render(Badges.BugHunter, "hunter"));
+        }
+        if (badges.early) {
+          res.props.children.push(render(Badges.EarlyUser, "early"));
+        }
+
+        return res;
+      }
+    );
+
+    UserProfileBadgeList.default.displayName = "UserProfileBadgeList";
+  }
+
+  async injectGuilds () {
+	const GuildHeader = await getModule([ 'AnimatedBanner', 'default' ]);
+	const GuildBadge = await getModuleByDisplayName('GuildBadge');
+  
+	inject('pc-badges-guilds-forked-header', GuildHeader.default, 'type', ([ props ], res) => {
+	  if (cache._guilds[props.guild.id]) {
+		res.props.children[0].props.children[0].props.children[0].props.children.unshift(
+		  React.createElement(Badges.Custom, {
+			...cache._guilds[props.guild.id],
+			tooltipPosition: 'bottom',
+			gap: false
+		  })
+		);
+	  }
+	  return res;
+	});
+  
+	inject('pc-badges-guilds-forked-tooltip', GuildBadge.prototype, 'render', function (_, res) {
+	  if (this.props.size && cache._guilds[this.props.guild.id]) {
+		return [
+		  React.createElement(Badges.Custom, {
+			...cache._guilds[this.props.guild.id],
+			tooltipPosition: 'bottom'
+		  }),
+		  res
+		];
+	  }
+	  return res;
+	});
+  
+	//const baseUrl = powercord.settings.get('backendURL', WEBSITE);
+
+	cache._guilds = {
+		"1014837057333497917": {
+			"name":"Kampelis",
+			"icon":"https://cdn.discordapp.com/emojis/694979272070266970.png"
+		},
+		"995705794312028260": {
+			"name":"ARCH",
+			"icon":"https://cdn.discordapp.com/attachments/772943180160958514/773261485790199809/OKAttic.png"
+		}
+	}
+  }
+
+  pluginWillUnload() {
+    unloadStyle(styleId);
+    uninject('pc-badges-users-forked-render');
+    uninject('pc-badges-users-forked-update');
+    uninject('pc-badges-users-forked-fetch');
+    uninject('pc-badges-guilds-forked-header');
+    uninject('pc-badges-guilds-forked-tooltip');
+
+    const containerClasses = getModule([ 'subscribeTooltipText' ], false);
+    const modalClasses = getModule([ 'topSectionNormal' ], false);
+    if (containerClasses) {
+      forceUpdateElement(`.${containerClasses.container}`);
+    }
+    if (modalClasses) {
+      const modalHeader = document.querySelector(`.${modalClasses.topSectionNormal} header`);
+      if (modalHeader) {
+        const instance = getOwnerInstance(modalHeader);
+        (instance._reactInternals || instance._reactInternalFiber).return.stateNode.forceUpdate();
+      }
+    }
+  }
+};
